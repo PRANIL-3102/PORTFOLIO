107a8cf5672ba30b91739bcddac30359cadd6d11
Pranil <pranilsinghamahapatra4@gmail.com>
2026-06-20 21:11:37 +0530

    Update Hero.jsx

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
           Hello, it's me <span>Pranil</span>
         </h1>
 
-        <h2>B.Tech CSE Student|Frontend Developer</h2>
+        <h2>B.Tech CSE Student | Frontend Developer</h2>
 
         <p>
           Passionate about React, JavaScript and building
