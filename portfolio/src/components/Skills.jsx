f0a077260af2d0b59585fd050a7c92c7d82363b4
Pranil <pranilsinghamahapatra4@gmail.com>
2026-06-20 21:25:06 +0530

    yay

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -1,26 +1,55 @@
-function Skills() {
-  const skills = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "Node.js",
-    "Git",
-    "GitHub",
-    "Supabase",
-  ];
+import {
+  FaReact,
+  FaHtml5,
+  FaCss3Alt,
+  FaGitAlt
+} from "react-icons/fa";
 
+function Skills() {
+const skills = [
+  {
+    name: "HTML",
+    icon: <FaHtml5 />,
+    desc: "Structure"
+  },
+
+  {
+    name: "CSS",
+    icon: <FaCss3Alt />,
+    desc: "Styling"
+  },
+
+  {
+    name: "React",
+    icon: <FaReact />,
+    desc: "Frontend"
+  },
+
+  {
+    name: "Git",
+    icon: <FaGitAlt />,
+    desc: "Version Control"
+  }
+];
   return (
     <section className="skills" id="skills">
       <h2>Skills</h2>
 
-      <div className="skills-container">
-        {skills.map((skill, index) => (
-          <div className="skill-card" key={index}>
-            {skill}
-          </div>
-        ))}
+<div className="skills-container">
+  {skills.map((skill) => (
+    <div className="skill-card">
+
+      <div className="skill-icon">
+        {skill.icon}
       </div>
+
+      <h3>{skill.name}</h3>
+
+      <p>{skill.desc}</p>
+
+    </div>
+  ))}
+</div>
     </section>
   );
 }
