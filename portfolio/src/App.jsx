6ff39d0c28657fbfba356bbdaed36cce4524c5b8
Pranil <pranilsinghamahapatra4@gmail.com>
2026-06-24 11:01:07 +0530

    a

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,14 +5,12 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Keyboard3D from "./components/Keyboard3D";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <About/>
-      <Keyboard3D/>
       <Skills/>
       <Projects/>
       <Contact/>
