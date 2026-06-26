550c90ed043f222e1518a1d6f91ff28772f663c7
Pranil <pranilsinghamahapatra4@gmail.com>
2026-06-26 21:20:07 +0530

    a

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,16 +5,18 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Intro from "./components/Intro0";
+import Intro from "./components/Intro";
 import { useState } from "react";
 function App() {
   const [darkMode, setDarkMode] = useState(true);
   const [themeTransition, setThemeTransition] = useState(false);
   const [showIntro, setShowIntro] = useState(true);
+  
+  if (showIntro) {
+    return <Intro onFinish={() => setShowIntro(false)} />;
+  }
+  
   return (
-    if (showIntro) {
-  return <Intro onFinish={() => setShowIntro(false)} />;
-}
     <div className={darkMode ? "dark-theme" : "light-theme"}>
     {themeTransition && (
   <div
